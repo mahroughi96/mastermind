0dc8bafd26a6ed3692758546b6296daf85902ea8
mahroughi <mahdis.froughi@gmail.com>
2019-03-27 00:27:22 +0430

    very beautiful working shit

--- mastermind/src/App.js
+++ mastermind/src/App.js
@@ -3,80 +3,101 @@ import { Button, Modal, Jumbotron } from "react-bootstrap";
 
 import "./App.css";
 
+const numberGenerator = () => {
+  return Math.floor(Math.random() * 100);
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.showModal = this.showModal.bind(this);
-    this.hideModal = this.hideModal.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
-    this.numberGenerator = this.numberGenerator.bind(this);
+    this.guessHandler = this.guessHandler.bind(this);
 
     this.state = {
-      userAnswer: "",
-      answer: this.numberGenerator(),
-      show: false
+      userAnswer: undefined,
+      answer: undefined,
+      show: false,
+      guesses: []
     };
   }
 
+  componentDidMount() {
+    this.playAgain();
+  }
+
   render() {
+
     return (
-      <div className="pageBody">
+      <div class="justify-content-md-center pageBody">
         <h1>my number is between (0-100), try guessing!</h1>
         <Jumbotron className="inputView">
           <input
             placeholder="your guess"
             onChange={e => this.setState({ userAnswer: e.target.value })}
-            value={this.state.userAnswer}
           />
-          <Button variant="outline-info" onClick={this.checkAnswer(this)}>
+          <Button
+            variant="info"
+            onClick={() => {
+              this.checkAnswer(this.state.userAnswer);
+              this.guessHandler(this.state.userAnswer);
+            }}
+          >
             submit your answer
           </Button>
+
+          {this.state.hint}
         </Jumbotron>
         <Modal show={this.state.show}>
           <p>YOU WIN</p>
-          <Button onClick={this.numberGenerator}>Play again!</Button>
+          <Button
+            variant="success"
+            onClick={() => {
+              this.playAgain();
+              this.handleToggle();
+            }}
+          >
+            Play again!
+          </Button>
         </Modal>
-        <h2 show={this.state.show}>{this.hint}</h2>
       </div>
     );
   }
 
-  showModal = () => {
-    this.setState({ show: true });
-  };
-
-  hideModal = () => {
-    this.setState({ show: false });
-  };
-
-  showHint = () => {
-    let hint = "";
-    this.setState({ show: true });
-    if (this.props === 1) {
-      hint = "go higher";
-    } else if (this.props === -1) {
-      hint = "go lower";
-    }
-    return hint;
-  };
-
-  numberGenerator() {
-    this.answer = Math.floor(Math.random() * 100);
-    console.log("answer generated: ", this.answer);
-    return this.answer;
-    
+  playAgain() {
+    const answer = numberGenerator();
+    console.log(answer);
+    this.setState({ answer, userAnswer: undefined });
   }
 
-  checkAnswer(userAnswer) {
-    console.log(userAnswer);
-    if (this.answer === Number(userAnswer)) {
-      console.log("hooooray");
-    } else if (this.answer > Number(userAnswer)) {
-      this.showHint(1);
-    } else if (this.answer < Number(userAnswer)) {
-      this.showHint(-1);
-    }
+  handleToggle = () => {
+    this.setState(prevState => {
+      return { show: !prevState.show };
+    });
+  };
+
+  setHint(id) {
+    let options = ["higher", "lower"];
+    this.setState({
+      hint: options[id]
+    });
   }
+
+  guessHandler = userAnswer => {
+    this.setState({guesses: userAnswer });
+  };
+
+  checkAnswer = userAnswer => {
+    console.log(this.state.guesses);
+    this.guessHandler();
+    if (this.state.answer === Number(userAnswer)) {
+      this.handleToggle();
+    } else if (this.state.answer > Number(userAnswer)) {
+      this.setHint(0);
+    } else if (this.state.answer < Number(userAnswer)) {
+      this.setHint(1);
+    }
+  };
 }
 
 export default App;
